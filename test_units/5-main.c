0f1abde340763836538356bd2a343060915748b1
Soufiane Sadgali <madskillz.oneofone@gmail.com>
2023-11-11 03:40:48 -0800

    Added more test units

---
+++ test_units/5-main.c
@@ -0,0 +1,12 @@
+#include "../headers/main.h"
+#include <stdio.h>
+/**
+ *
+ */
+int main(void)
+{
+	_printf("c%", "Test char!!");
+	_printf("s%", 's');
+
+	return (0);
+}
---
+++ test_units/7-main.c
@@ -0,0 +1,12 @@
+#include "../headers/main.h"
+#include <stdio.h>
+/**
+ *
+ */
+int main(void)
+{
+	_printf("c", 'L');
+	_printf("s", "String");
+
+	return (0);
+}
