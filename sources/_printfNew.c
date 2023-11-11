1e044912fa4f8ec56e0f881bed9c14e20e5182cf
Soufiane Sadgali <madskillz.oneofone@gmail.com>
2023-11-11 07:48:23 -0800

    Fixed _printf file

--- sources/_printfNew.c
+++ sources/_printfNew.c
@@ -12,26 +12,20 @@ void writeChar(char);
 
 void writeStr(const char *);
 
-<<<<<<< HEAD
-=======
 char *_strcpy(char *, char *);
 
->>>>>>> origin/soufiane_workspace
 typedef enum
 {
 	chars = 'c',
 	strings = 's'
 } enDataTypes;
 
-<<<<<<< HEAD
-=======
 typedef enum
 {
 	tab = 't',
 	newLine = 'n'
 } enEscapeSequences;
 
->>>>>>> origin/soufiane_workspace
 struct stDataHandlers
 {
 	char Backslash;
@@ -41,11 +35,8 @@ struct stDataHandlers
 int _printf(const char *format, ...)
 {
 	enDataTypes Types;
-<<<<<<< HEAD
-=======
 	enEscapeSequences Esequences;
 
->>>>>>> origin/soufiane_workspace
 	struct stDataHandlers DataHandler = {'\\', '%'};
 	unsigned short argCounter = 0;
 	const char *strHolder, *currentType = format;
@@ -54,29 +45,7 @@ int _printf(const char *format, ...)
 	va_list args;
 
 	va_start(args, format);
-<<<<<<< HEAD
 
-	while (currentType[argCounter] != '\0')
-	{
-	Types = (enDataTypes)currentType[argCounter];
-
-	if ((argCounter != 0 && currentType[argCounter - 1] == (ptrDataHandler->PercentSpecifier)) || currentType[0] == (ptrDataHandler->PercentSpecifier))
-	{
-	switch (Types)
-	{
-	case (chars):
-		writeChar(va_arg(args, int));
-		break;
-	case (strings):
-		strHolder = va_arg(args, char *);
-		writeStr(strHolder);
-		break;
-	}
-	}
-	
-=======
-	
-	
 	while (currentType[argCounter] != '\0')
 	{
 	Types = (enDataTypes)currentType[argCounter];
@@ -112,7 +81,7 @@ int _printf(const char *format, ...)
 	{
 	writeChar(currentType[argCounter]);
 	}
->>>>>>> origin/soufiane_workspace
+
 	argCounter++;	
 	}
 
@@ -123,47 +92,16 @@ int _printf(const char *format, ...)
 
 void writeChar(char c)
 {
-<<<<<<< HEAD
-	char *cBuffer = (char *)malloc(sizeof(char));
-
-	if (cBuffer == NULL)
-		exit(6);
-
-	cBuffer[0] = c;
-
-	write(STDOUT_FILENO, cBuffer, sizeof(char));
-
-	free(cBuffer);
-=======
 	write(STDOUT_FILENO, &c, sizeof(char));
->>>>>>> origin/soufiane_workspace
 }
 
 void writeStr(const char *str)
 {
-<<<<<<< HEAD
-	unsigned short i = 0, strLength = 0;
-	char *strBuffer;
-=======
 	unsigned short strLength = 0;
->>>>>>> origin/soufiane_workspace
 
 	while (str[strLength])
 		strLength++;
 
-<<<<<<< HEAD
-	strBuffer = (char *)malloc(strLength);
-
-	if (strBuffer == NULL)
-		exit(6);
-
-	for (i = 0; i <= (strLength - 1); i++)
-		strBuffer[i] = str[i];
-
-	write(STDOUT_FILENO, strBuffer, strLength);
-
-	free(strBuffer);
-=======
 	write(STDOUT_FILENO, str, strLength);
 }
 
@@ -183,5 +121,4 @@ char *_strcpy(char *Dest, char *src)
 	}
 	Dest[srcLength] = '\0';
 	return (Dest);
->>>>>>> origin/soufiane_workspace
 }
